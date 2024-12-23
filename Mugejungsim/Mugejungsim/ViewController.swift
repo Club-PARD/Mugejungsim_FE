4e1b68fbbf84c6ac6ad41a48668dd9c104febcc2
Seunghun-Hyeon <tmdgns4970@handong.ac.kr>
2024-12-23 15:39:53 +0900

    Splash 추가

--- Mugejungsim/Mugejungsim/ViewController.swift
+++ Mugejungsim/Mugejungsim/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 배경 색상 설정
+        view.backgroundColor = UIColor.systemBlue
+        
+        // 중앙 텍스트 추가
+        let welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome to Snowball App!"
+        welcomeLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        welcomeLabel.textColor = UIColor.white
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(welcomeLabel)
+        
+        // 텍스트 레이아웃 설정
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
 
