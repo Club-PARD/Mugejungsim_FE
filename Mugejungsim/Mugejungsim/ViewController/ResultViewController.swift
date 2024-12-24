83ad76b21b82c8e16e10134bb47963d80618ef77
Hyeonhak_Do <ehgusgkr6@naver.com>
2024-12-24 19:38:26 +0900

    [Feat] : ResultViewController에 버튼 추가

--- Mugejungsim/Mugejungsim/ViewController/ResultViewController.swift
+++ Mugejungsim/Mugejungsim/ViewController/ResultViewController.swift
@@ -5,9 +5,16 @@
 //  Created by 도현학 on 12/24/24.
 //
 
+/*
+ 필요사항
+ 1. 중앙 이미지 수정 필요
+ */
+
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    // MARK: - Properties (UI Elements)
     
     let memoryLabel: UILabel = {
         let label = UILabel()
@@ -30,30 +37,48 @@ class ResultViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    
+    let saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("오브제 저장하기", for: .normal)
+        button.backgroundColor = UIColor(hex: "#19FF5E")
+        button.setTitleColor(.black, for: .normal)
+        button.isEnabled = true // 버튼 활성화
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
-    // 버튼 선언
     let openPreviewButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "snow"), for: .normal) // 수정되어야함
+        button.setImage(UIImage(named: "moments"), for: .normal)
         button.imageView?.contentMode = .scaleAspectFit
         button.backgroundColor = .clear
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
+    // MARK: - Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
         
         // 버튼 액션 연결
         openPreviewButton.addTarget(self, action: #selector(openUSDZPreviewController), for: .touchUpInside)
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
 
-        // 서브뷰 추가
         view.addSubview(memoryLabel)
         view.addSubview(touchLabel)
         view.addSubview(openPreviewButton)
+        view.addSubview(saveButton)
+        
+        setConstraints()
+    }
 
+    // MARK: - Constraints Setup
+    
+    private func setConstraints(){
         // 제약 조건 설정
         NSLayoutConstraint.activate([
             // openPreviewButton 위치 (화면 정중앙)
@@ -61,17 +86,28 @@ class ResultViewController: UIViewController {
             openPreviewButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             openPreviewButton.widthAnchor.constraint(equalToConstant: 150), // 버튼 너비
             openPreviewButton.heightAnchor.constraint(equalToConstant: 150), // 버튼 높이
-            
-            // touchLabel 위치 (openPreviewButton 위)
+            // touchLabel
             touchLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             touchLabel.bottomAnchor.constraint(equalTo: openPreviewButton.topAnchor, constant: -20),
-            
-            // memoryLabel 위치 (touchLabel 위)
+            // memoryLabel
             memoryLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            memoryLabel.bottomAnchor.constraint(equalTo: touchLabel.topAnchor, constant: -10)
+            memoryLabel.bottomAnchor.constraint(equalTo: touchLabel.topAnchor, constant: -10),
+            // saveButton
+            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            saveButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
-
+    
+    // MARK: - Button Actions
+    
+    // 저장 버튼 푸시 함수
+    @objc func saveButtonTapped(){
+        print("성공!")
+    }
+    
+    // USDZPreviewViewController로 이동
     @objc func openUSDZPreviewController() {
         let USDZPreviewVC = USDZPreviewViewController() // USDZModal 페이지 호출
         USDZPreviewVC.modalPresentationStyle = .fullScreen
